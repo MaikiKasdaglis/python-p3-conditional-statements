55f7279a29315a008d9eba632040d4964a7795a8
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-07-21 12:38:09 -0600

    finishes lab

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,42 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if username.lower() == 'admin' and password == "12345":
+        return "Access granted"
+    else:
+       return "Access denied"
+    
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+   dict_map = {
+       temperature < 40 : "brisk",
+       temperature >= 40 and temperature <65 : "a little chilly",
+       temperature >= 85 : "too dang hot",
+   }
+   response = dict_map.get(True, "perfect")
+   return (f"It's {response} out there!")
+
 
 def fizzbuzz(num):
-    # your code here
-    pass
+   if num % 5 == 0 and num % 3 == 0:
+    return "FizzBuzz"
+   elif num %  5 == 0:
+    return "Buzz"
+   elif num % 3 == 0:
+    return "Fizz"
+   else:
+      return num
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        print("Invalid operation!")
+        return None
+
